f39022cb0861976e1f4402f02420962c994967c4
Aditya Pai <paiadityashwetha319@gmail.com>
2021-10-30 18:46:00 +0530

    update

--- BackTrack.java
+++ BackTrack.java
@@ -1,8 +1,17 @@
-// import java.util.*;
+import java.util.*;
 
 public class BackTrack {
     public static void main(String[] args) {
-        int a = 10, b = 20;
-        System.out.println("hello world " + a + b);
+
+    }
+
+    // leetcode 1947
+    public static int maxCompatibilitySum(int[][] students, int[][] mentors) {
+        Map<Integer, int[]> map1 = new HashMap<>();
+        Map<Integer, int[]> map2 = new HashMap<>();
+        for (int i = 0; i < students.length; i++) {
+            map1.put(i, students[i]);
+            map2.put(i, mentors[i]);
+        }
     }
 }
